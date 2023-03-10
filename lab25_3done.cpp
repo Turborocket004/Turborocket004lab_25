bf080f59c5a95fc241460c5a4f0c294707908d54
Turborocket004 <120168125+Turborocket004@users.noreply.github.com>
2023-03-10 16:55:58 +0700

    Update and rename lab25_3.cpp to lab25_3done.cpp

--- lab25_3done.cpp
+++ lab25_3done.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 //Write function count() here.
+int count(int a[],int n){
+    set<int> setnum;
+    for(int i = 0; i < n; i++){
+        setnum.insert(a[i]);
+    }
+    return setnum.size();
+    
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
